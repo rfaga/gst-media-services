4d05eac2768ad85ccaa0cdae60e24a47d0d29b63
robertofaga <robertofaga@3c753797-934b-0410-a049-f1d884abcb7f>
2008-08-08 03:59:35 +0000

    interface now is working right, integrated with gstms services and running the Transcoder service. At gstms, Transcoder is just using a default conversion, ignoring Profiles. Next version I will integrate them so conversion will work rightly.

--- gstms.py
+++ gstms.py
@@ -25,15 +25,21 @@ import time
 import xml.dom.minidom as xmldom
 
 
+#media types
 VIDEO_TYPE = 1
 AUDIO_TYPE = 2
 DATA_TYPE = 3
 
-EMPTY = 'EMPTY'
+#messages about conversion
+CONVERSION_FINISHED = 31
+CONVERSION_FAILED = 32
+CONVERSION_RUNNING = 33
+
+EMPTY = gst.caps_from_string('EMPTY')
 ANY = 'ANY'
 
 FILE_SOURCE = "gnomevfssrc"
-
+DECODEBIN = "decodebin2"
 ################################# Misc #######################################
 
 class File(object):
@@ -54,15 +60,18 @@ class File(object):
         self.mimetype = None
     
     def on_discover(self, discover, success):
-        #if success:
-        self.set_mimetype(discover.mimetype.split(",")[0])
+        try:
+            self.set_mimetype(discover.mimetype.split(",")[0])
+        except:
+            #if ocurred an error, file is data file, unrecognized by discoverer
+            pass
         if discover.is_video:
             self.mediatype = VIDEO_TYPE
         elif discover.is_audio:
             self.mediatype = AUDIO_TYPE
         else:
             self.mediatype = DATA_TYPE
-        print discover.is_video, discover.is_audio, success
+        #print discover.is_video, discover.is_audio, success
 
     def set_mimetype(self, mimetype):
         """
@@ -88,29 +97,29 @@ class ElementFactory:
         self.element_factory = element_factory
         pads = element_factory.get_static_pad_templates()
         self.name = element_factory.get_name()
-        self.sinks = gst.caps_from_string(EMPTY)
-        self.sources = gst.caps_from_string(EMPTY)
+        self.sinks = EMPTY.copy()
+        self.sources = EMPTY.copy()
+        self.klass = element_factory.get_klass().lower()
         if not pads:
             return None
 
         #now, for each pad template, classify as SINK or SOURCE
         for pad in pads:
             if pad.direction == gst.PAD_SINK:
-                self.sinks.union(pad.get_caps())
+                self.sinks = self.sinks.union(pad.get_caps())
             elif pad.direction == gst.PAD_SRC:
-                self.sources.union(pad.get_caps())
+                self.sources = self.sources.union(pad.get_caps())
             #if, for any unknown reason, it is another type of direction, ignore it.
 
-    def check_klass(self, klass):
-        return klass in self.klass
-
-    def check(self, sink_caps, source_caps):
+    def check(self, sink_caps, source_caps, klasses):
         """
         checks if this ElementFactory can generate a gst element for desired
         pipeline
 
         sink_caps -- sink caps which element must have, at least
         source_caps -- source caps which elements must have, at least
+        klasses -- a list of klasses which this element must have. empty lists
+            to accept any element.
 
         returns True or False, if this EF can be used or not.
         """
@@ -119,14 +128,19 @@ class ElementFactory:
         #XXX: I'm not sure about this, but I can't see why uniting sinks with
         # themselves can not work, as different sinks normally are different.
 
-        if sink_caps == EMPTY:
-            sink_condition = True
-        if self.sources.intersect(source_caps) != EMPTY:
-            source_condition = True
+        #check gst klasses, if False I can discard this element right now
+        for klass in klasses:
+            if klass not in self.klass:
+                return False
 
         if source_caps == EMPTY:
             source_condition = True
-        if self.sinks.intersect(sink_caps) != EMPTY:
+        elif self.sources.intersect(source_caps) != EMPTY:
+            source_condition = True
+
+        if sink_caps == EMPTY:
+            sink_condition = True
+        elif self.sinks.intersect(sink_caps) != EMPTY:
             sink_condition = True
 
         return sink_condition and source_condition
@@ -162,7 +176,7 @@ def load_registry():
 load_registry()
 
 
-def get_possible_elements(sink_caps, source_caps, favorites):
+def get_possible_elements(sink_caps, source_caps, favorites, klasses):
     """
     get all possible elements, considering the caps and trying 
     first the favorites
@@ -175,18 +189,20 @@ def get_possible_elements(sink_caps, source_caps, favorites):
     option in first place and worst in last.
     """
     possible_list = []
+    favorites.sort(None,None,True) #reverse sort
     for ef in elements_factory:
-        if ef.check(sink_caps, source_caps):
+        if ef.check(sink_caps, source_caps, klasses):
             possible_list += [ef]
     for pe in possible_list:
         if pe.name in favorites:
             possible_list.remove(pe)
             possible_list.insert(0, pe)
-
     return possible_list
+    #TODO: correctly construction of possible_elements
+    #return favorites
 
 ########################### XML Profile Parser ###############################
-class Profile(object):
+class Profile:
     """
     A profile class, with helpful properties and methods.
     This profile means variables and pipeline to a Gstreamer conversion.
@@ -217,12 +233,18 @@ class Profile(object):
         self.description = xmlnode.getElementsByTagName("description")[0].firstChild.nodeValue
         self.output_file_extension = xmlnode.getElementsByTagName("output-file-extension")[0].firstChild.nodeValue
 
-        process = xmlnode.getElementsByTagName("process")[0]
-        elements = process.childNodes
-        for e in elements:
+        self.elements = {}
+        elementsxml= xmlnode.getElementsByTagName("element")
+        for e in elementsxml:
             element = self.probe_element(e)
             if not element:
-               error("no element found for '%s'"%self.name)
+               error("no element found for '%s'"%e.getAttribute("id"))
+            else:
+                self.elements.update({element.name: element})
+
+        #TODO: construct pipeline conversion
+        self.transcoder = Transcoder(self.id)
+        
 
     def probe_element(self, elementxml):
         """
@@ -230,41 +252,54 @@ class Profile(object):
 
         return element in gstms.Element format
         """
-        source_caps = elementxml.getElementsByTagName("source")[0].firstChild.nodeValue
-        sink_caps = elementxml.getElementsByTagName("sink")[0].firstChild.nodeValue
+        source_caps = gst.caps_from_string(elementxml.getElementsByTagName("source")[0].firstChild.nodeValue)
+        sink_caps = gst.caps_from_string(elementxml.getElementsByTagName("sink")[0].firstChild.nodeValue)
 
         #let's read recommended-elements, this can help our element finder
-        recommendedsxml = elementxml.getElementsByTagName("recommended-elements")
+        recommendedsxml = elementxml.getElementsByTagName("recommended-element")
         favorites = []
         for r in recommendedsxml:
             favorites += [r.firstChild.nodeValue]
+ 
+        klasses = []
+        klassesxml = elementxml.getElementsByTagName("klass")
+        for klass in klassesxml:
+            klasses += [klass.firstChild.nodeValue.lower()]
 
         #now we get the possible element in gstms.Element format.
         # oh, and this element can be None if no element was found
-        elements = get_possible_elements(sink_caps, source_caps, favorites)
+        elements = get_possible_elements(sink_caps, source_caps, favorites, klasses)
         if not elements:
             return None
         
-        #element.type = elementxml.getAttribute("type")
-        #TODO: store the possible_elements list to try each one
+        element = elements[0] #XXX: for now, the first of list is the chosen one
+        element.type = elementxml.getAttribute("type")
         
         #TODO: parse properties options from elementxml
-        return elements[0]
+        return element
+    
+    def __str__(self):
+        return self.name
 
+    def transcode(self, file_path, updater=None):
+        self.transcoder.updater = updater
+        self.transcoder.play_file(file_path)
 
-    #XXX: In the future, do a write method to write a profile into a file, so editing is easier.
+    #XXX: In the future, do a write method to write a profile into a file, so editing will be easier.
 
 
 def get_profiles():
     """
     Get all possible profiles from XML databases.
+
+    return: a tuple with audio profiles and video profiles lists.
     """
     #TODO: I need to read XMLs from the /usr/share folder (or some other system folder) and from a home folder (like ~/.gstms in UNIX-like)
     HOME_PATH = os.path.expanduser("~/.gstms")
     SYSTEM_PATH = "profiles/" #local path, same of source file, for now...
 
     profiles_path = os.listdir(SYSTEM_PATH)
-    profiles = []
+    profiles_audio, profiles_video = [], []
     for path in profiles_path:
         fullpath = os.path.join(os.path.abspath(SYSTEM_PATH), path)
         try:
@@ -273,21 +308,22 @@ def get_profiles():
             error("file %s is not a valid XML file"%fullpath)
             continue
 
-        try:
-            possible_profiles = doc.firstChild.childNodes
-            for profile in possible_profiles:
+        #try:
+        possible_profiles = doc.firstChild.childNodes
+        for profile in possible_profiles:
                 #if node is a child
-                if profile.nodeType == 1:
-                    profiles += [Profile(profile)]
-        except:
-            error("xml file at '%s' is not in EncodingProfiles format. \n\
-                    Python error: '%s'"%(fullpath, str(sys.exc_info()[0]))
-    return profiles
+            if profile.nodeType == 1:
+                p = Profile(profile)
+                if p.type == VIDEO_TYPE:
+                    profiles_video += [p]
+                elif p.type == AUDIO_TYPE:
+                    profiles_audio += [p]
+        #except:
+        #    error("xml file at '%s' is not in EncodingProfiles format. \n\
+        #            Python error: '%s'"%(fullpath, str(sys.exc_info()[0])))
+    return profiles_audio, profiles_video
 
-
-##################### UNUSED CODE #######################
-# For now this code above is not being used
-# but I'll let them there because I'll use pipelines...
+##################### Pipeline model #######################
 class Pipeline:
     """
     Pipeline class, to simplify other pipeline uses
@@ -309,15 +345,24 @@ class Pipeline:
         #print "Incoming message! - '%s'"%t
         # If process ended
         if t == gst.MESSAGE_EOS:
-            self.stop()
-        #elif t == gst.MESSAGE_ERROR:
+            #print message.parse_eos()
+            self.finish()
+        elif t == gst.MESSAGE_STATE_CHANGED:
+            #print message.parse_state_changed()
+            pass
+        
+        elif t == gst.MESSAGE_ERROR:
             #TODO: send error message to a right place
-            #print message.parse_error()
+            print message.parse_error()
+
+        elif t == gst.MESSAGE_NEW_CLOCK:
+            print message.parse_new_clock()
+
         elif t == gst.MESSAGE_TAG:
             self.found_tag(message.parse_tag()) 
 
     def found_tag(self, message):
-        print message.keys()
+        pass#print message.keys()
 
     def play(self):
         bus = self.pipeline.get_bus()
@@ -326,7 +371,6 @@ class Pipeline:
         self.watch_id = bus.connect("message", self.on_message)
         
         self.pipeline.set_state(gst.STATE_PLAYING)
-        self.state = gst.STATE_PLAYING
 
     def stop(self):
         try:
@@ -337,83 +381,60 @@ class Pipeline:
         except:
             pass #do nothing
         self.pipeline.set_state(gst.STATE_NULL)
-        self.state = gst.STATE_NULL
 
+    def finish(self):
+        #implement it!
+        pass
 
-class TypeFinder(Pipeline):
-    """
-    A class to make an object which find every type of files / streams.
-    """
+class Transcoder(Pipeline):
 
-    def __init__(self, file):
-        """
-        Constructor
-        """
-        Pipeline.__init__(self, "typefinder")
+    def __init__(self, name):
+        Pipeline.__init__(self, name)
+        self.filesource = gst.element_factory_make(FILE_SOURCE, "filesource")
 
-        self.source = gst.element_factory_make(FILE_SOURCE, "filesource")
-        self.pipeline.add(self.source)
+        self.audioconvert = gst.element_factory_make("audioconvert", "audioconvert")
+        self.audiosink = gst.element_factory_make("fakesink", "audiosink")
 
-        self.current_file = file
-        self.source.set_property("location", file.uri)
-        typefind = gst.element_factory_make("typefind", "typefind")
-        typefind.connect("have-type", self.have_type)
-        self.pipeline.add(typefind)
+        self.pipeline.add(self.filesource, self.audioconvert, self.audiosink)
+        
+        gst.element_link_many(self.audioconvert, self.audiosink)
+        self.updater = None
 
-        fakesink = gst.element_factory_make("fakesink", "fakesink")
-        self.pipeline.add(fakesink)
+    def new_decoded_pad(self, decodebin, pad, last):
+        #print "decoded! %s"%pad.get_caps().to_string()
+        #pad.link(self.sink)
+        spad = self.audioconvert.get_static_pad("sink")
+        if pad.get_caps().intersect(spad.get_caps()) != "EMPTY":
+            pad.link(spad)
+        else:
+            fs = gst.element_factory_make("fakesink", "fksink")
+            self.pipeline.add(fs)
+            spad = fs.get_static_pad("sink")
+            pad.link(spad)
+        self.pads += [(pad, spad)]
 
-        gst.element_link_many(self.source, typefind, fakesink)
+    def play_file(self, file_path):
+        self.filesource.set_property("location", file_path)
+        self.decode = gst.element_factory_make(DECODEBIN, "decodebin")
+        self.decode.connect("new-decoded-pad", self.new_decoded_pad)
+        self.pipeline.add(self.decode)
+
+        self.filesource.link(self.decode)
+        
+        self.pads = []
         self.play()
 
-    def have_type(self, typefind, probability, caps):
-        """
-        Method to happen when a have-type signal is given from typefind element
+    def finish(self):
+        self.stop()
+        self.pipeline.remove(self.decode)
+        for pad, spad in self.pads:
+            pad.unlink(spad)
+        self.filesource.unlink(self.decode)
+        #self.decode.disconnect("new-decoded-pad")
+        del self.decode
+        if self.updater:
+            self.updater(CONVERSION_FINISHED)
 
-        typefind -- The typefind element
-        probability -- Probability of the type found
-        caps -- caps with information of mimetype
-        """
-        self.caps = caps.to_string().split(",")[0]
-        if "current_file" in dir(self):
-            self.current_file.set_mimetype(self.caps)
-            del self
-
-
-class Decoder_(Pipeline):
-    """
-    A pipeline factory to decode files and get info from them
-    """
-
-    def __init__(self, file):
-        """
-        Constructor
-        """
-        
-        Pipeline.__init__(self, "decoder")
-
-        self.source = gst.element_factory_make(FILE_SOURCE, "filesource")
-        self.pipeline.add(self.source)
-        
-        decodebin = gst.element_factory_make("decodebin2", "decodebin")
-        decodebin.connect("new-decoded-pad", self.new_decoded_pad)
-        self.pipeline.add(decodebin)
-
-        self.fakesink = gst.element_factory_make("fakesink", "fakesink")
-        self.pipeline.add(self.fakesink)
-
-        gst.element_link_many(self.source, decodebin)
-        self.current_file = file
-        self.source.set_property("location", file.uri)
-        self.play()
-
-    def new_decoded_pad(self, element, pad, boolean):
-        """
-        element -- Current element (decodebin)
-        pad -- new pad created
-        boolean -- ahm?
-        """
-        print "called new_decoded_pad: %s"%pad.get_caps().to_string().split(",")[0]
-        self.current_file.add_caps(pad.get_caps().to_string())
-        #gst.message_new_custom(gst.MESSAGE_EOS, self.pipeline, gst.Structure("finish it"))
-
+#import gtk
+#t = Transcode("a"); t.play_file("/home/faga/video.wmv")
+#gtk.main()
