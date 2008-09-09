93536a0f04e89f8520da1d16f11d82338c37cf21
robertofaga <robertofaga@3c753797-934b-0410-a049-f1d884abcb7f>
2008-09-09 00:47:56 +0000

    new C code being imported

---
+++ cgstms/src/main.c
@@ -0,0 +1,25 @@
+/*
+*******************************************************************************
+* This file is part of gst-media-services.                                    *
+* Copyright (C) 2008 Roberto Faga Jr, Stefan Kost and Gstreamer team.         *
+*                                                                             *
+* This program is free software: you can redistribute it and/or modify        *
+* it under the terms of the GNU Lesser General Public License as published    *
+* by the Free Software Foundation, version 3.                                 *
+*                                                                             *
+* This program is distributed in the hope that it will be useful,             *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+* GNU Lesser General Public License for more details.                         *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program.  See LICENSE file.                                 *
+*******************************************************************************
+*/
+
+#include "gstms.h"
+
+int main (int argc, char *argv[]){
+
+  return 0;
+}
